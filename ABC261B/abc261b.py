9d319dd3f89902ce4639210ca4c5b97c7390f513
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-07-24 13:49:29 +0900

    ABC261B

---
+++ ABC261B/abc261b.py
@@ -0,0 +1,15 @@
+N = int(input())
+A = [input() for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        if A[i][j] == "W" and A[j][i] != "L":
+            print("incorrect")
+            exit()
+        elif A[i][j] == "L" and A[j][i] != "W":
+            print("incorrect")
+            exit()
+        elif A[i][j] == "D" and A[j][i] != "D":
+            print("incorrect")
+            exit()
+print("correct")

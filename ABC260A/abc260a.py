4ecac701d0234ff6a2faae117dea292cbe24a8c1
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-07-24 13:17:08 +0900

    計算結果を変数に代入

--- ABC260A/abc260a.py
+++ ABC260A/abc260a.py
@@ -2,9 +2,10 @@ import collections
 
 
 S = input()
-if len(set(S)) == 3:
+unique_letter = len(set(S))
+if unique_letter == 3:
     print(S[0])
-elif len(set(S)) == 2:
+elif unique_letter == 2:
     # 重複してない文字を探す
     ans = [k for k, v in collections.Counter(S).items() if v == 1]
     print(ans[0])

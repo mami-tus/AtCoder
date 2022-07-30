5db6b203b4ff9161ffc6e32b17e263cf590930b9
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-07-30 23:49:00 +0900

    シンプルな解法に修正

--- ABC261A/abc261a.py
+++ ABC261A/abc261a.py
@@ -1,19 +1,5 @@
 red_l, red_r, blue_l, blue_r = map(int, input().split())
-# red_lとblue_lを比較して小さい方の数直線を固定する
-if red_l <= blue_l:
-    fixed_color_l = red_l
-    fixed_color_r = red_r
-    not_fixed_color_l = blue_l
-    not_fixed_color_r = blue_r
-else:
-    fixed_color_l = blue_l
-    fixed_color_r = blue_r
-    not_fixed_color_l = red_l
-    not_fixed_color_r = red_r
-
-if fixed_color_r <= not_fixed_color_l:
-    print(0)
-elif fixed_color_r < not_fixed_color_r:
-    print(fixed_color_r - not_fixed_color_l)
-else:
-    print(not_fixed_color_r - not_fixed_color_l)
+max_l = max(red_l, blue_l)
+min_r = min(red_r, blue_r)
+d = min_r - max_l
+print(d if d > 0 else 0)

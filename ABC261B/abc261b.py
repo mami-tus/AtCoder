1851f269f795a7eee64b622bdbe2efcd36d576d7
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-07-30 23:29:50 +0900

    調べる回数を減らすよう修正

--- ABC261B/abc261b.py
+++ ABC261B/abc261b.py
@@ -1,15 +1,18 @@
+import itertools
+
+
 N = int(input())
 A = [input() for _ in range(N)]
+combinations = list(itertools.combinations(range(N), 2))
 
-for i in range(N):
-    for j in range(N):
-        if A[i][j] == "W" and A[j][i] != "L":
-            print("incorrect")
-            exit()
-        elif A[i][j] == "L" and A[j][i] != "W":
-            print("incorrect")
-            exit()
-        elif A[i][j] == "D" and A[j][i] != "D":
-            print("incorrect")
-            exit()
+for i, j in combinations:
+    if A[i][j] == "W" and A[j][i] != "L":
+        print("incorrect")
+        exit()
+    elif A[i][j] == "L" and A[j][i] != "W":
+        print("incorrect")
+        exit()
+    elif A[i][j] == "D" and A[j][i] != "D":
+        print("incorrect")
+        exit()
 print("correct")

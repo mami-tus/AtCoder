e93b11bc97bd287fb4d1ce05a9e07036bab51a00
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-08-06 20:52:39 +0900

    計算結果を変数に取り出す

--- ABC262A/abc262a.py
+++ ABC262A/abc262a.py
@@ -1,9 +1,10 @@
 Y = int(input())
-if Y % 4 == 1:
+calc = Y % 4
+if calc == 1:
     print(Y + 1)
-elif Y % 4 == 2:
+elif calc == 2:
     print(Y)
-elif Y % 4 == 3:
+elif calc == 3:
     print(Y + 3)
 else:
     print(Y + 2)

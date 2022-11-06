69e54aadfb52431c58af894cd90232881e649803
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-11-06 22:09:22 +0900

    ジェネレータ式にする

--- ABC274B/abc274b.py
+++ ABC274B/abc274b.py
@@ -1,9 +1,8 @@
-import collections
-
-
 H, W = map(int, input().split())
-C = [list(input()) for _ in range(H)]
-for j in range(W):
-    column = [i[j] for i in C]
-    count = collections.Counter(column)
-    print(count["#"])
+C = (list(input()) for _ in range(H))
+count = [0] * W
+for i in C:
+    for j in range(W):
+        if i[j] == "#":
+            count[j] += 1
+print(*count)

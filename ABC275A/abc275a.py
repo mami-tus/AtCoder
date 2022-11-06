b1a1280fa8fe875cb4ea55acc306c9a11e5d581b
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-11-06 22:59:11 +0900

    for文を1回にする

--- ABC275A/abc275a.py
+++ ABC275A/abc275a.py
@@ -1,3 +1,11 @@
 N = int(input())
 H = list(map(int, input().split()))
-print(H.index(max(H)) + 1)
+i = 1
+max = 0
+max_i = 0
+for h in H:
+    if max < h:
+        max = h
+        max_i = i
+    i += 1
+print(max_i)

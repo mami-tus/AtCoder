81c0556acb48bbb763324f8d63a50dc46104d5c2
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-11-06 23:58:02 +0900

    メモ化再帰で実装

--- ABC275D/abc275d.py
+++ ABC275D/abc275d.py
@@ -1,9 +1,16 @@
-def func(k: int) -> int:
-    if k == 0:
-        return 1
-    else:
-        return func(k // 2) + func(k // 3)
+class Memo:
+    def __init__(self):
+        self.memo = {}
+
+    def func(self, k: int) -> int:
+        if k == 0:
+            return 1
+        if k in self.memo.keys():
+            return self.memo[k]
+        self.memo[k] = self.func(k // 2) + self.func(k // 3)
+        return self.memo[k]
 
 
 N = int(input())
-print(func(N))
+memo = Memo()
+print(memo.func(N))

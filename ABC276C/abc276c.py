93f03a0f8e3f84682d04951a77b2164b6562f32c
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-11-13 21:48:12 +0900

    単調増加を探して実装

--- ABC276C/abc276c.py
+++ ABC276C/abc276c.py
@@ -1,12 +1,19 @@
-import itertools
-
-
 n = int(input())
 p = list(map(int, input().split()))
-permutations = list(itertools.permutations(range(1, n + 1), n))
-index = 0
-for i in permutations:
-    if p == list(i):
-        break
-    index += 1
-print(*list(permutations)[index - 1])
+
+
+# 単調増加でない要素を探す
+i = n - 2
+while p[i] < p[i + 1]:
+    i -= 1
+
+# p[i]より小さい要素を後ろから探す
+j = n - 1
+while p[i] < p[j]:
+    j -= 1
+
+p[i], p[j] = p[j], p[i]
+x = p[: i + 1]
+y = p[i + 1 :]
+y.sort(reverse=True)
+print(*x, *y)

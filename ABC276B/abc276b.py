714bdea64106cc8731e600d6f73183890a8884b7
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-11-13 11:09:19 +0900

    Cityクラス作成

--- ABC276B/abc276b.py
+++ ABC276B/abc276b.py
@@ -1,11 +1,22 @@
-n, m = map(int, input().split())
-roads = [list(map(int, input().split())) for _ in range(m)]
-box = [[] for _ in range(n)]
+class City:
+    def __init__(self, num: int) -> None:
+        self.num = num
 
-for a, b in roads:
-    box[a - 1].append(b)
-    box[b - 1].append(a)
-for i in box:
-    new_i = sorted(i)
-    new_i.insert(0, len(i))
-    print(*new_i)
+    def get_connected_cities(self, roads: list[tuple[int, int]]) -> list[int]:
+        connected_cities = []
+        for city_a, city_b in roads:
+            if city_a == self.num:
+                connected_cities.append(city_b)
+            elif city_b == self.num:
+                connected_cities.append(city_a)
+        return connected_cities
+
+
+n, m = map(int, input().split())
+roads = [tuple(map(int, input().split())) for _ in range(m)]
+
+for num in range(1, n + 1):
+    city = City(num)
+    connected_cities = sorted(city.get_connected_cities(roads))
+    connected_cities.insert(0, len(connected_cities))
+    print(*connected_cities)

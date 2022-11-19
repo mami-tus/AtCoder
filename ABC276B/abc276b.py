5c771aa1cad23f899705f891cfed3e8bda3cb1e0
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-11-19 19:53:35 +0900

    クラスのプロパティ修正

--- ABC276B/abc276b.py
+++ ABC276B/abc276b.py
@@ -1,22 +1,19 @@
 class City:
-    def __init__(self, num: int) -> None:
+    def __init__(self, num: int, connected_cities: list[int]) -> None:
         self.num = num
-
-    def get_connected_cities(self, roads: list[tuple[int, int]]) -> list[int]:
-        connected_cities = []
-        for city_a, city_b in roads:
-            if city_a == self.num:
-                connected_cities.append(city_b)
-            elif city_b == self.num:
-                connected_cities.append(city_a)
-        return connected_cities
+        self.connected_cities = connected_cities
 
 
 n, m = map(int, input().split())
 roads = [tuple(map(int, input().split())) for _ in range(m)]
 
 for num in range(1, n + 1):
-    city = City(num)
-    connected_cities = sorted(city.get_connected_cities(roads))
-    connected_cities.insert(0, len(connected_cities))
-    print(*connected_cities)
+    connected_cities = []
+    for city_a, city_b in roads:
+        if city_a == num:
+            connected_cities.append(city_b)
+        elif city_b == num:
+            connected_cities.append(city_a)
+    city = City(num, connected_cities)
+    connected_cities = sorted(city.connected_cities)
+    print(len(connected_cities), *connected_cities)

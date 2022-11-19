855d873b1ba4ce77223b81b94bb4dba1cc0cc7cb
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-11-19 23:50:15 +0900

    クラス修正

--- ABC276B/abc276b.py
+++ ABC276B/abc276b.py
@@ -6,14 +6,12 @@ class City:
 
 n, m = map(int, input().split())
 roads = [tuple(map(int, input().split())) for _ in range(m)]
+cities = [City(num, []) for num in range(1, n + 1)]
 
-for num in range(1, n + 1):
-    connected_cities = []
-    for city_a, city_b in roads:
-        if city_a == num:
-            connected_cities.append(city_b)
-        elif city_b == num:
-            connected_cities.append(city_a)
-    city = City(num, connected_cities)
-    connected_cities = sorted(city.connected_cities)
-    print(len(connected_cities), *connected_cities)
+for a, b in roads:
+    city_a = cities[a - 1]
+    city_b = cities[b - 1]
+    city_a.connected_cities.append(city_b.num)
+    city_b.connected_cities.append(city_a.num)
+for city in cities:
+    print(len(city.connected_cities), *sorted(city.connected_cities))

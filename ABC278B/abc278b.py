2c0dba7aca275a78f75ec941c8b9424210b04f27
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-11-23 02:13:06 +0900

    1時間おきに調べるパターン追加

--- ABC278B/abc278b.py
+++ ABC278B/abc278b.py
@@ -1,20 +1,14 @@
-import datetime
+from datetime import datetime, timedelta
 
 
 class Clock:
-    def __init__(self, time: datetime.datetime):
+    def __init__(self, time: datetime):
         self.time = time
 
     def is_misjudged_time(self):
         h_zero = str(self.time.hour).zfill(2)
         m_zero = str(self.time.minute).zfill(2)
-        a, b, c, d = (
-            h_zero[0],
-            h_zero[1],
-            m_zero[0],
-            m_zero[1],
-        )
-        a, b, c, d = int(a), int(b), int(c), int(d)
+        a, b, c, d = int(h_zero[0]), int(h_zero[1]), int(m_zero[0]), int(m_zero[1])
         if a * 10 + c <= 23 and b * 10 + d <= 59:
             return True
         else:
@@ -22,11 +16,13 @@ class Clock:
 
 
 h, m = map(int, input().split())
-clock = Clock(datetime.datetime(2020, 1, 1, h, m))
+clock = Clock(datetime(2020, 1, 1, h, m))
 
 while True:
     if clock.is_misjudged_time():
         print(clock.time.hour, clock.time.minute)
         break
+    elif 5 < clock.time.hour < 9 or 15 < clock.time.hour < 19:
+        clock.time += timedelta(hours=1)
     else:
-        clock.time += datetime.timedelta(minutes=1)
+        clock.time += timedelta(minutes=1)

381851ef1ff723ef4fe871c0bc1e7cbceb94bfa9
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-11-22 23:30:53 +0900

    インスタンスメソッド追加

--- ABC276B/abc276b.py
+++ ABC276B/abc276b.py
@@ -3,6 +3,9 @@ class City:
         self.num = num
         self.connected_cities = connected_cities
 
+    def add_connected_city(self, city_no: int) -> None:
+        self.connected_cities.append(city_no)
+
 
 n, m = map(int, input().split())
 roads = [tuple(map(int, input().split())) for _ in range(m)]
@@ -11,7 +14,7 @@ cities = [City(num, []) for num in range(1, n + 1)]
 for a, b in roads:
     city_a = cities[a - 1]
     city_b = cities[b - 1]
-    city_a.connected_cities.append(city_b.num)
-    city_b.connected_cities.append(city_a.num)
+    city_a.add_connected_city(city_b.num)
+    city_b.add_connected_city(city_a.num)
 for city in cities:
     print(len(city.connected_cities), *sorted(city.connected_cities))

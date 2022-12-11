c306ffda3ad09f955cca07568727e96ccab90556
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-12-12 00:28:03 +0900

    count使う

--- ABC280A/abc280a.py
+++ ABC280A/abc280a.py
@@ -2,7 +2,5 @@ h, w = map(int, input().split())
 s = [list(input()) for _ in range(h)]
 count = 0
 for i in s:
-    for j in i:
-        if j == "#":
-            count += 1
+    count += i.count("#")
 print(count)

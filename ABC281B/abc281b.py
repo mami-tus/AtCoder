7a679515c3543629d6090524e9a7c47fbbf01210
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-12-13 23:35:26 +0900

    isupperに修正

--- ABC281B/abc281b.py
+++ ABC281B/abc281b.py
@@ -2,8 +2,8 @@ s = input()
 if (
     len(s) != 8
     or s[1] == "0"
-    or not s[0].isalpha()
-    or not s[7].isalpha()
+    or not s[0].isupper()
+    or not s[7].isupper()
     or not s[1:7].isdecimal()
 ):
     print("No")
--- ABC281B/abc281b_2.py
+++ ABC281B/abc281b_2.py
@@ -1,6 +1,6 @@
 s = input()
 try:
-    if len(s) != 8 or s[1] == "0" or not s[0].isalpha() or not s[7].isalpha():
+    if len(s) != 8 or s[1] == "0" or not s[0].isupper() or not s[7].isupper():
         raise Exception
     int(s[1:7])
     print("Yes")

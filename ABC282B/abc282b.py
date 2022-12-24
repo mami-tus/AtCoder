c801070844f116298888f982abc8ce8515cd16d1
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-12-24 12:56:19 +0900

    ビット演算を使う

--- ABC282B/abc282b.py
+++ ABC282B/abc282b.py
@@ -2,15 +2,15 @@ import itertools
 
 
 n, m = map(int, input().split())
-s = [list(input()) for _ in range(n)]
-
+s_list = [list(input()) for _ in range(n)]
+new_s = []
+for s in s_list:
+    for i in range(m):
+        s[i] = "1" if s[i] == "o" else "0"
+    bin_s = "".join(s)
+    new_s.append(int(bin_s, 2))
 count = 0
 for x, y in itertools.combinations(range(n), 2):
-    is_solved = True
-    for i in range(m):
-        if s[x][i] != "o" and s[y][i] != "o":
-            is_solved = False
-            break
-    if is_solved:
+    if (new_s[x] | new_s[y]) == int("1" * m, 2):
         count += 1
 print(count)

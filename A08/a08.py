41c81292bcbd3140e0ce9a7e125feead94571359
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2023-02-19 22:02:20 +0900

    refactor: 🦄  mapにしてメモリを節約する

--- A08/a08.py
+++ A08/a08.py
@@ -1,7 +1,6 @@
 h, w = map(int, input().split())
 x = [list(map(int, input().split())) for _ in range(h)]
 q = int(input())
-queries = [list(map(int, input().split())) for _ in range(q)]
 
 # 縦方向の累積和
 accm = [[0] * (w + 1) for _ in range(h + 1)]  # 0の行と列を追加して初期化
@@ -13,5 +12,7 @@ for i in range(1, h + 1):
 for i in range(1, h + 1):
     for j in range(1, w + 1):
         accm[i][j] += accm[i][j - 1]
-for a, b, c, d in queries:
+
+for _ in range(q):
+    a, b, c, d = map(int, input().split())
     print(accm[c][d] - accm[a - 1][d] - accm[c][b - 1] + accm[a - 1][b - 1])

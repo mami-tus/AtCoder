952cc984b860ebd9a5df2c9bcdbeaf2938eca311
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2023-02-19 20:56:40 +0900

    refactor: 🦄  ansを配列に持たないようにする

--- A07/a07.py
+++ A07/a07.py
@@ -13,7 +13,7 @@ for i in range(n):
     day_before_last[l_list[i]] += 1
     day_before_last[r_list[i] + 1] -= 1
 
-ans = [0] * (d + 2)  # 最初と最後に0を追加して初期化
+ans = 0  # 最初と最後に0を追加して初期化
 for d in range(1, d + 1):
-    ans[d] = ans[d - 1] + day_before_last[d]
-    print(ans[d])
+    ans += day_before_last[d]
+    print(ans)

15329deca122a03f44488924a49afb2990219f6c
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2023-02-26 21:52:22 +0900

    feat: ✨  abc284c
    C - Count Connected Components

---
+++ ABC284C/abc284c.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+import sys
+
+sys.setrecursionlimit(10000)  # 再起回数の設定
+
+
+def dfs(v: int, visited: list[bool], graph: list[int]) -> None:
+    visited[v] = True
+    for neighbor in graph[v]:
+        if not visited[neighbor]:
+            dfs(v=neighbor, visited=visited, graph=graph)
+
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (n + 1)
+count = 0
+for v in range(1, n + 1):
+    if not visited[v]:
+        dfs(v=v, visited=visited, graph=graph)
+        count += 1
+print(count)
